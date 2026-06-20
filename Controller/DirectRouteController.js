d6a0f939abd5eb28cd86a0299777fea1755bed38
SANJAY DUTT <duttmagic5@gmail.com>
2026-06-20 17:07:59 +0530

    minor issue fix

--- Controller/DirectRouteController.js
+++ Controller/DirectRouteController.js
@@ -1,9 +1,13 @@
 exports.DirectAccess = (req,res,next) => {
 	
 	  if (req.session && req.session.AdminLoggedIn) {
-      return res.status(200).json();
+      return res.status(200).json({
+        message:"Successfull"
+      });
     }  else if (req.session && req.session.TeacherloggedIn) {
-     return  res.status(200).json();
+     return  res.status(200).json({
+      message:"Successfull"
+     });
   } else {
     res.status(403).json({
       message: "Log In First",
